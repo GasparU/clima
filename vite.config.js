df5e1b92546e5c43a7909affab1817b6454951d4
Jose Gaspar <sainc2016@gmail.com>
2025-03-07 11:29:07 -0500

    actualizado 2025

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import nodePolyfills from "rollup-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), tailwindcss(), react()],
+  plugins: [, tailwindcss(), react()],
 });
